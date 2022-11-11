463f0c324acbd56038d36b3501d5bb300e5cb758
adrian-dragos <adrian123795@gmail.com>
2022-11-11 11:02:35 +0200

    add item 6

--- tema1/A.cpp
+++ tema1/A.cpp
@@ -3,7 +3,31 @@
 using namespace std;
 
 class A {       
-  public:             
+  private:             
     int number;        
-    string string; 
+    std::string str; 
+
+    A(int number, std::string str) {
+        this->number = number;
+        this->str = str;
+    }
+
+    A(const A& a) {
+        cout << "Calling copy constructor\n";
+        this->number = a.number;
+        this->str = a.str;
+    }
+
+    private: 
+        A(A && obj);
+
+    private:
+        A& operator=(A&&);
+
+
 };
+
+int main() {
+    cout << "hello";
+    return 0;
+}
--- tema1/B.cpp
+++ tema1/B.cpp
@@ -2,8 +2,29 @@
 #include <string> 
 using namespace std;
 
-class A {       
-  public:             
-    int number;        
-    string string; 
+class B {       
+    public:             
+        string str; 
+
+    B(int number, std::string str) {
+        this->str = str;
+    }
+
+    private: B(const B& b);
+
+
+    B(B && b) {
+        cout << "Calling move constructor\n";
+        this->str = b.str;
+        b.str = "";
+    } 
+
+    private:
+        B& operator=(const B&);
+
 };
+
+int main() {
+    cout << "hello";
+    return 0;
+}
