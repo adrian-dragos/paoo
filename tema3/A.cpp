c094d24b14129528e17bf61db275237b012506d0
adrian-dragos <adrian123795@gmail.com>
2022-12-08 19:20:26 +0200

    add code imporments

--- tema3/A.cpp
+++ tema3/A.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <random>
+#include <functional>
 
 using namespace std;
 
@@ -8,29 +10,41 @@ using namespace std;
 class Pokemon {
     public:
         int number;
-        string str;
+        string name;
 
     public:
-        Pokemon(int number, string str): number(number), str(str) { }
+        Pokemon(int number, string name): number(number), name(name) { }
 
     // item 14 prohibit copy
     private: 
         Pokemon(const Pokemon& pokomon);
 
     public:
-        Pokemon* createObjectA(int number, string str) {
-            Pokemon *pokemon = new Pokemon(number, str);
-            return pokemon;
+        Pokemon* createPikachu() {
+            Pokemon *pikachu = new Pokemon(1, "Picachu");
+            return pikachu;
         }
 
     public:
-        void fightPickachu() {
-            std::auto_ptr<Pokemon> pokemon(createObjectA(0, "Pikachu"));
+        bool fightPickachu() {
+            std::auto_ptr<Pokemon> pokemon(createPikachu());
+	        int random = rand() % 2;
+            return random;
     }
 };
 
 int main() {
-    Pokemon pokemon(1, "Eevee");
-    pokemon.fightPickachu();
+    srand((unsigned) time(NULL));
+
+    Pokemon eevee(2, "Eevee");
+    for (int i = 0; i < 10; i++) {
+        cout << eevee.name;
+        if (eevee.fightPickachu()) {
+            cout << " won";
+        } else {
+            cout << " lost";
+        }
+        cout << " against Pikachu." << endl;
+    }
     return 0;
 }
